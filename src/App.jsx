e31c43b2cfc43c597822bfa116b74177554083e4
jaswanthinvally <jaswanthvenkat001@gmail.com>
2023-12-20 19:04:06 +0530

    chart created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Navbar from './assets/Navbar/Navbar'
+import Charts from './assets/Charts';
 
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <div className='w-2/3'>
+      <Charts/> 
+      </div>
+
     </div>
   );
 }
